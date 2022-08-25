93e01b91cef6acdd73e4d4a0e641712d64d2d956
arielmitchell <ariel.l.makowski@gmail.com>
2022-08-25 10:21:43 -0400

    Initial set-up

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,26 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String addEvents(Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("Raleigh's Birthday");
+        events.add("Rowan's Birthday");
+        events.add("Apple Festival");
+
+        model.addAttribute("events", events);
+
+        return "events/index";
+    }
+}
